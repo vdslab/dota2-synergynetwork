38e0270ed41d25c4afe48cb9c1b7e540c84db33f
BlueKoopa0127 <bluekoopa@me.com>
2022-12-19 12:04:32 +0900

    add network function

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,22 @@ export default function Home(data) {
   return (
     <div>
       <h1>Dota2 SynergyNetwork</h1>
+      <div>
+        <h2>HeroSynergyNetwork</h2>
+        <Network data={data} />
+      </div>
     </div>
   )
 }
+
+function Network(data) {
+  // todo ここにネットワークを描画するコードを書く @樋口
+  // dataは高見が取ってきたjsonデータ
+  return (<div>
+    ネットワーーーークの描画
+  </div>);
+}
+
 export async function getStaticProps() {
   console.log("getStaticProps");
   const data = await getHeroCombinationWinLose();
