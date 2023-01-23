cbdb58935aee4e03551a677a0d6304baceb6954a
BlueKoopa0127 <bluekoopa@me.com>
2023-01-23 13:40:27 +0900

    delete forceDirected, openDotaAPI add jsonData,mdsAPI

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,123 +1,13 @@
-export async function request(query) {
-    const response = await fetch(`https://api.opendota.com/api/explorer?sql=${query}`);
+export async function request(jsonData) {
+    const response = await fetch("https://mds-xd44p75ygq-de.a.run.app/", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(jsonData),
+    });
+    console.log(response);
     const json = await response.json();
     return json;
-}
-
-export async function getHeroCombinationWinLose() {
-    const patches = await getPatchData(4);
-    console.log(patches);
-    var array = [];
-    for (const element of patches) {
-        console.log(element.patch);
-        var data = await getDataByPatch(element);
-        array.push(...data);
-        console.log("records: " + array.length);
-        await sleep(2000);
-    }
-
-    await compileData(array);
-    await calcWin_rate(array);
-    if (!array) {
-        array = [];
-    }
-    var response = { rows: array }
-    console.log(response);
-    return response;
-}
-
-export async function calcWin_rate(array) {
-    console.log(array[0]);
-    array.forEach((obj) => { obj.winrate = obj.win / obj.count });
-}
-
-export async function getRecords(start, end, patch) {
-    let response = null;
-    for (let i = 0; i < 10; i++) {
-        response = await request(`SELECT table3.hero1,table3.hero2, COUNT(table3.radiant_win=true OR NULL) AS win, COUNT(table3.radiant_win=false OR NULL) AS lose,  COUNT(table3.table1ID) FROM (SELECT tableA.match_id AS table1ID, tableB.match_id table2ID, matches.radiant_win, tableA.hero_id AS hero1, tableB.hero_id AS hero2 FROM (SELECT picks_bans.match_id, picks_bans.hero_id, picks_bans.is_pick,picks_bans.team, match_patch.patch FROM picks_bans, match_patch WHERE picks_bans.match_id = match_patch.match_id AND match_patch.patch = '${patch.patch}' ORDER BY picks_bans.match_id DESC LIMIT ${end} OFFSET ${start}) AS tableA INNER JOIN (SELECT picks_bans.match_id, picks_bans.hero_id, match_patch.patch ,picks_bans.is_pick, picks_bans.team FROM picks_bans, match_patch WHERE picks_bans.match_id = match_patch.match_id AND match_patch.patch = '${patch.patch}' ORDER BY picks_bans.match_id DESC LIMIT ${end} OFFSET ${start}) AS tableB ON tableA.match_id = tableB.match_id INNER JOIN matches ON matches.match_id = tableA.match_id where tableA.team = tableB.team  AND tableA.is_pick = true AND tableB.is_pick = true AND tableA.hero_id < tableB.hero_id ORDER BY tableA.match_id DESC) AS table3 GROUP BY table3.hero1, table3.hero2 ORDER BY table3.hero1, table3.hero2
-        `);
-        if (response.rows) {
-            break;
-        }
-        console.log(response);
-        await sleep(10000);
-    }
-    return response;
-
-}
-
-export async function compileData(array) {
-    var new_array = array.reduce(function (result, current) {
-        var obj = result.find(function (element) { return current.hero1 == element.hero1 && current.hero2 == element.hero2 });
-        if (obj) {
-            obj.win = obj.win + current.win;
-            obj.lose = obj.lose + current.lose;
-            obj.count = obj.count + current.count;
-        } else {
-            const data2 = {
-                hero1: current.hero1,
-                hero2: current.hero2,
-                win: current.win,
-                lose: current.lose,
-                count: current.count
-            }
-            result.push(data2);
-        }
-        return result;
-
-    }, [])
-    return new_array;
-}
-
-export async function getMatchCount(patch) {
-    let response = null;
-    for (let i = 0; i < 10; i++) {
-        response = await request(`SELECT COUNT(*) FROM (SELECT * FROM  matches INNER JOIN  match_patch ON matches.match_id = match_patch.match_id AND match_patch.patch = '${patch.patch}'  ORDER BY matches.match_id DESC) AS table1`);
-        if (response.rows) {
-            break;
-        }
-        console.log("response(matchCount):" + response);
-        await sleep(10000);
-    }
-    return response;
-}
-
-export async function getDataByPatch(patch) {
-    var response = [];
-    const oneTimeAmount = 1000;
-    const records = await getMatchCount(patch);
-    console.log("records:" + records);
-    let recordCount = 0;
-    recordCount = records.rows[0].count;
-    console.log("count" + recordCount);
-
-    let num = 0;
-    while (num <= recordCount) {
-        const data = await getRecords(num, oneTimeAmount, patch);
-        await sleep(200);
-        if (data.rows) {
-            response.push(...data.rows);
-            console.log(num + "=" + data.rows[0]);
-            console.log(num + "=" + data.rows.length);
-        } else {
-            console.log(num + "=" + "miss" + data);
-        }
-        num += oneTimeAmount;
-    }
-
-    return response;
-}
-export async function getPatchData(limit) {
-    let data = await request(`SELECT DISTINCT match_patch.patch FROM match_patch ORDER BY patch DESC LIMIT ${limit}`);
-    return data.rows;
-}
-
-export async function getHeroData() {
-    return await request("SELECT id, localized_name AS heroName FROM heroes ");
-}
-
-function sleep(s) {
-    return new Promise(function (resolve) {
-        setTimeout(function () { resolve() }, s)
-    })
 }
--- pages/index.js
+++ pages/index.js
@@ -1,127 +1,33 @@
-import { getHeroCombinationWinLose, getHeroData } from "./api/api";
 import * as d3 from "d3";
 import { useEffect, useRef, useState } from "react";
-import { count, index } from "d3";
+import { request } from "./api/api"
 
-export default function Home({ data }) {
-  console.log(data);
+export default function Home({ jsonData, posData }) {
+  console.log(jsonData);
   return (
     <div>
       <h1>Dota2 SynergyNetwork</h1>
       <div>
         <h2>HeroSynergyNetwork</h2>
-
+        <ScatterPlot posData={posData} />
       </div>
     </div>
   );
 }
 
-function Network({ _nodesData, _linksData }) {
-  const nodesCount = 10;
-  const [nodesData, setNodesData] = useState(_nodesData);
-  const [linksData, setLinksData] = useState(_linksData);
-  const width = 1000;
-  const height = 1000;
+function ScatterPlot({ posData }) {
+  console.log(posData);
 
-  useEffect(() => {
-
-    const startSimulation = (nodes, links) => {
-      const simulation = d3
-        .forceSimulation()
-        .force(
-          "link",
-          d3
-            .forceLink()
-            .id((d) => d.id)
-            .distance(function (d) {
-              return (1 - d.winRate) * 500;
-            })
-            .iterations(128)
-        )
-        .force(
-          "collide",
-          d3
-            .forceCollide()
-            .radius(function (d) {
-              return d.r;
-            })
-            .strength(0.7)
-            .iterations(128)
-        )
-        .force("charge", d3.forceManyBody().strength(-2000))
-        .force("center", d3.forceCenter(width / 2, height / 2));
-      simulation.nodes(nodesData).on("tick", ticked);
-      simulation
-        .force("link")
-        .links(linksData)
-        .id(function (d) {
-          return d.index;
-        });
-      function ticked() {
-        setNodesData(nodes.slice());
-        setLinksData(links.slice());
-      }
-    };
-    startSimulation(nodesData, linksData);
-    const winRateAndDist = linksData
-      .map((d) => {
-        const di = Math.sqrt(
-          (d.source.x - d.target.x) ** 2 + (d.source.y - d.target.y) ** 2
-        );
-        return {
-          id: d.id,
-          s: d.source.heroName,
-          t: d.target.heroName,
-          wi: d.winRate,
-          dist: di,
-        };
-      })
-      .sort((a, b) => a.dist - b.dist);
-    console.log(winRateAndDist);
-  }, [nodesCount]);
-  return (
-    <ZoomableSVG width={width} height={height}>
-      {linksData.map((data, index) => {
-        return (
-          <g key={index}>
-            <line
-              x1={data.source.x}
-              x2={data.target.x}
-              y1={data.source.y}
-              y2={data.target.y}
-              stroke="black"
-            ></line>
-          </g>
-        );
-      })}
-
-      {nodesData.map((data, index) => {
-        return (
-          <g key={index}>
-            <circle cx={data.x} cy={data.y} r={data.r} fill="blue"></circle>
-            <text
-              textAnchor="middle"
-              stroke="black"
-              fill="Red"
-              fontSize={"10px"}
-              x={data.x}
-              y={data.y}
-            >
-              {data.heroName}
-            </text>
-          </g>
-        );
-      })}
-    </ZoomableSVG>
-  );
+  // todo ZoomableSVGを適用した散布図を描く
 }
 
 export async function getStaticProps() {
   const fs = require('fs');
-  const data = JSON.parse(fs.readFileSync("/Users/ikko/大学/尾上ゼミ/dota2-synergynetwork/dota2Data.json"))
+  const jsonData = JSON.parse(fs.readFileSync("/Users/ikko/大学/尾上ゼミ/dota2-synergynetwork/dota2Data.json"));
+  const posData = await request(jsonData);
 
   return {
-    props: { data },
+    props: { jsonData, posData },
   };
 }
 
