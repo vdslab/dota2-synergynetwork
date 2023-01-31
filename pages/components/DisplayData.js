1e73c4c1525036c1a5979868bf124fe0d8b24110
torii1100 <nira398@outlook.jp>
2023-02-01 08:16:34 +0900

    add_match_count

--- pages/components/DisplayData.js
+++ pages/components/DisplayData.js
@@ -5,13 +5,17 @@ import Image from 'next/image'
 export const DisplayData = ({ selectedNode, posData, _jsonData }) => {
     let displayIcons = []
     let win_rate = null
+    let match_count = null
     let text = " "
+    let pickText = " "
     if (selectedNode[0] != -1 && selectedNode[1] != -1) {
         const combinationData = _jsonData.getHeroCombinationWinLose.find(element =>
             element.hero1 == selectedNode[0] && element.hero2 == selectedNode[1] ||
             element.hero1 == selectedNode[1] && element.hero2 == selectedNode[0])
         win_rate = combinationData.winrate
+        match_count = combinationData.count
         text = "勝率:"
+        pickText = "試合数:"
     }
     const imageSize = 10
     return (
@@ -39,6 +43,8 @@ export const DisplayData = ({ selectedNode, posData, _jsonData }) => {
                 }
             </Grid>
             <h3>{text}{win_rate ? (Math.round(win_rate * 1000) / 10) : ""}{"%"}</h3>
+            <h3>{pickText}{ match_count ? match_count : ""}{"試合"}</h3>
+
         </Box>
     );
 }
