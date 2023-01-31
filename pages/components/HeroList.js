b8843329ec7551e133080a4d4893231c760e355d
torii1100 <nira398@outlook.jp>
2023-02-01 04:37:35 +0900

    add_heroList

--- pages/components/HeroList.js
+++ pages/components/HeroList.js
@@ -1,9 +1,9 @@
 import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
 export const HeroList = ({posData}) =>{
-    const imageSize=5;
+    const imageSize=8;
     return(
-    <Box sx={{overflow: 'auto', width:400, height:400}}>
+    <Box sx={{overflow: 'auto', width:399, height:1000}}>
         <Grid container spacing={2}>
             {
                 posData.map((element)=>{
