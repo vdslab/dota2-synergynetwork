3d2138411e19df308234e6b70ff71ef6b9c73ade
torii1100 <nira398@outlook.jp>
2023-02-01 12:00:31 +0900

    bug_fix

--- pages/components/DisplayData.js
+++ pages/components/DisplayData.js
@@ -37,7 +37,7 @@ export const DisplayData = ({ selectedNode, setSelectedNode, posData, _jsonData,
                     selectedNode.map((element, index) => {
                         if (element != -1) {
                             return (
-                                <Grid item key={element}>
+                                <Grid item key={index}>
                                     <Image
                                         src={posData.find((e) => { return (e.id == element) }).image}
                                         height={imageSize * 9}
